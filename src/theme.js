6e4a5ca0f80be3f31c231ffed9a0bfcbbcad681b
AnshC <63086888+AnshC@users.noreply.github.com>
2021-12-01 15:00:04 +0530

    theme color added

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,8 @@ export const theme = {
     colors: {
         bg: "#060A07",
         primary: "#55F680",
-        text: "#D7FEE1"
+        secondary: "#30FFCE",
+        text: "#D7FEE1",
+        bgTransparent: "rgba(0,0,0,0.2)"
     }
 }
