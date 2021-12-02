77f22c1a47b4ada594b1095ca8ba7f0e1de9badb
AnshC <63086888+AnshC@users.noreply.github.com>
2021-12-02 16:10:06 +0530

    made the auth components

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -4,17 +4,17 @@ import AuthForm from '../../components/AuthForm'
 import Link from 'next/link'
 import Gradient from '../../components/Gradient'
 
-export default function Signup () {
+export default function Signin () {
     return (
         <div sx={{ backgroundColor: "bg" }} className={styles.main}>
             <Gradient />
             <div className={styles.container}>
                 <AuthForm 
-                    head="Sign Up."
-                    description="Create an account to set up your streaks!"
-                    alt={<>Already have an account? <Link href="/auth/signin"><a>Login.</a></Link></>}
-                    button="Sign Up"
-                    fn="signup"
+                    head="Sign In."
+                    description="Sign in to check out your streaks!"
+                    alt={<>Don't have an account? <Link href="/auth/signup"><a>Create one.</a></Link></>}
+                    button="Sign In"
+                    fn="signin"
                 />
             </div>
         </div>
