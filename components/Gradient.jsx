63728ae8200a8d75e58128687d22dd6355893bc9
AnshC <63086888+AnshC@users.noreply.github.com>
2021-12-02 16:09:37 +0530

    made the bg gradient into a usable component

---
+++ components/Gradient.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Components.module.css'
+export default function Gradient () {
+    return (
+        <>
+            <div className={styles.gradient_green}></div>
+            <div className={styles.gradient_blue}></div>
+        </>
+    )
+}
